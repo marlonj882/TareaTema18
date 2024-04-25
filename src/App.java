98c07568cbca14e18f14ef682ad311bf1a53626c
marlonj882 <ttxd77@gmail.com>
2024-04-25 15:54:43 +0200

    commit2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Feature2");
+        System.out.println("Hello, Feature2");
     }
 }
