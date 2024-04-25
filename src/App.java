aa1a655a2dcfaa4c0a0ba6636546af7e53319c46
marlonj882 <ttxd77@gmail.com>
2024-04-25 16:03:08 +0200

    commitdespuesdemerge

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Hotfix");
+        System.out.println("Hello, Hotfix");
     }
 }
